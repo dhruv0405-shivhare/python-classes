430f3d56f8e252018964d44bfad03532583cf37d
Dhruv <dshivhare661@gmail.com>
2024-11-16 15:10:16 +0530

    pattern

--- transfer statement/transfer.py
+++ transfer statement/transfer.py
@@ -5,11 +5,36 @@
 #break
 #pass
 
+# n = int (input("enter any no."))
+# i = 1
+# while i<=n:
+#     if i ==5:
+#     i = i + 1    
+#         continue 
+#     else:
+#         print(i)
+#         i = i + 1
+
+
 n = int (input("enter any no."))
 i = 1
 while i<=n:
     if i ==5:
-        continue
+        pass 
     else:
         print(i)
         i = i + 1
+
+
+
+
+
+n = int (input("enter any no."))
+i = 1
+while i<=n:
+     if i ==5:
+         i = i + 1    
+         continue 
+     else:
+         print(i)
+         i = i + 1
