e92029dd86e8cb562570adc217c9a1817f0c6c77
Dhruv <dshivhare661@gmail.com>
2024-11-16 14:56:42 +0530

    pattern

---
+++ transfer statement/transfer.py
@@ -0,0 +1,15 @@
+
+# transfer statement
+
+# continue
+#break
+#pass
+
+n = int (input("enter any no."))
+i = 1
+while i<=n:
+    if i ==5:
+        continue
+    else:
+        print(i)
+        i = i + 1
