a663a5f24a1e2f7caa6b8f1ce80dc39dd922553c
Dhruv <dshivhare661@gmail.com>
2024-11-19 15:28:52 +0530

    function

---
+++ function/function.py
@@ -0,0 +1,10 @@
+def add(x,y,z):
+    z = x+y+z
+    #print(z) 
+    return z
+p = int (input("enter 1st value"))
+q = int (input("enter 2nd value"))
+r = int (input("enter 3rd value"))
+
+x = add(y=r,x=p,z=q)
+print (x)
