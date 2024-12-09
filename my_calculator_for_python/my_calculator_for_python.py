72d87185ccf32c634d85216462096ea1f1bd8ba8
Dhruv <dshivhare661@gmail.com>
2024-12-09 15:26:29 +0530

    aliasing

---
+++ my_calculator_for_python/my_calculator_for_python.py
@@ -0,0 +1,8 @@
+x=10
+y=20
+
+def add_calculator(x,y):
+    # return x+y
+    print(x+y)
+def sub(x,y):
+    return x-y
---
+++ my_calculator_for_python/new.py
@@ -0,0 +1,11 @@
+# import my_calculator_for_python as m  #aliasing --> this is use when function name is long and repeating again and again then we use aliasing
+
+# print(m.x)
+# print(m.y)
+
+
+from my_calculator_for_python import add_calculator as ac
+# data = (ac(10,20))
+# print(data)
+
+ac(10,20)
