68d9b0daf280fccadf4295835b5e2cc0b0ea7200
Dhruv <dshivhare661@gmail.com>
2024-12-14 15:29:32 +0530

    local_variable

---
+++ local variable/local_variable.py
@@ -0,0 +1,31 @@
+# x=50
+# class student:
+#     global x
+#     x=10
+
+#     def add ():
+        
+#         print(x)
+#         y=20
+#         print(y)
+#         print(x)
+#     def add1 ():
+#         print(x)
+# obj=student
+# obj.add()
+# obj.add1()
+# print(x)        
+
+################################################################
+
+x=70
+class bhopal:
+    global x
+    x=20
+    def add():
+        print(x)
+        y=50
+        print(y)
+obj=bhopal
+obj.add()
+print (x)        
