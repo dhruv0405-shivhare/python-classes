36b54386117d813aea9e123d3fb2646ddb7b5df3
Dhruv <dshivhare661@gmail.com>
2024-12-19 15:41:14 +0530

    polymorphism

---
+++ methods/polymorphism.py
@@ -0,0 +1,35 @@
+# polymorphism --> same name function with different arugments.
+
+my_list = [10,20,'dhruv']
+my_string = "dhruv"
+
+print(len(my_string))
+print(len(my_list))
+
+# Type  -->
+#1. method overloading :
+#2. method overriding  :
+
+
+# error code
+# class A :
+#     def add (self,x,y):
+#         return x+y
+#     def add (self,x,y,z):
+#         return x+y+z
+# obj = A()
+# obj.add(10,20)    
+
+
+class A :
+    def add (self,*n):
+        sum = 0
+        for i in n :
+            sum = sum + i
+        return sum
+obj = A()
+x=obj.add(10,20)
+print(x)  
+
+y=obj.add(10,20,30)
+print(y)  
