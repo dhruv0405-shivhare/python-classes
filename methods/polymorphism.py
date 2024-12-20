9e1eb4655d65007109cd4e8a0a83453aebda42ce
Dhruv <dshivhare661@gmail.com>
2024-12-20 14:58:03 +0530

    over riding

--- methods/polymorphism.py
+++ methods/polymorphism.py
@@ -1,10 +1,10 @@
 # polymorphism --> same name function with different arugments.
 
-my_list = [10,20,'dhruv']
-my_string = "dhruv"
+# my_list = [10,20,'dhruv']
+# my_string = "dhruv"
 
-print(len(my_string))
-print(len(my_list))
+# print(len(my_string))
+# print(len(my_list))
 
 # Type  -->
 #1. method overloading :
@@ -21,15 +21,33 @@ print(len(my_list))
 # obj.add(10,20)    
 
 
-class A :
-    def add (self,*n):
-        sum = 0
-        for i in n :
-            sum = sum + i
-        return sum
-obj = A()
-x=obj.add(10,20)
-print(x)  
+# class A :
+#     def add (self,*n):
+#         sum = 0
+#         for i in n :
+#             sum = sum + i
+#         return sum
+# obj = A()
+# x=obj.add(10,20)
+# print(x)  
 
-y=obj.add(10,20,30)
-print(y)  
+# y=obj.add(10,20,30)
+# print(y)  
+
+
+
+#over riding -->
+
+class c:
+    def display (self):
+        print("from class c")
+
+class d(c):
+    def  display(self):
+         print("from class d")
+    def p_display(self):
+        self.display
+        super().display()
+obj = d()
+obj.p_display()
+obj.display()                   
